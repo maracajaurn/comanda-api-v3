47c68a6fa07707d1bd61c8bfc225f4561d2cd152
JackSSads <contatojacksonsouza@hotmail.com>
2024-12-31 00:47:55 -0300

    ajustando organização do código

--- src/api/router/user.js
+++ src/api/router/user.js
@@ -6,8 +6,8 @@ router.get("/", async (req, res) => {
     try {
         const result = await UserService.service_query_select_all();
         res.status(200).send(result);
-    } catch (err) {
-        logger.error("Error fetching users:", err);
+    } catch (error) {
+        logger.error("Error fetching users:", error);
         res.status(500).send({ message: "Erro ao buscar os usuário" });
     };
 });
@@ -18,41 +18,39 @@ router.get("/:user_id", async (req, res) => {
     try {
         const result = await UserService.service_query_select_by_id(user_id);
         res.status(200).send(result);
-    } catch (err) {
-        logger.error("Error fetching user:", err);
+    } catch (error) {
+        logger.error("Error fetching user:", error);
         res.status(500).send({ message: "Erro ao buscar os usuários" });
     };
 });
 
+// TODO: Criar validação para e-mail
 router.post("/", async (req, res) => {
     const { username, email, password, func } = req.body;
 
-    const data = {
-        username, email, password, func
-    };
-
     try {
+        const data = { username, email, password, func };
+
         await UserService.service_query_insert_user(data);
         res.status(201).send({ message: "Usuário criado com sucesso" });
-    } catch (err) {
-        logger.error("Error fetching user:", err);
+    } catch (error) {
+        logger.error("Error fetching user:", error);
         res.status(500).send({ message: "Erro ao criar o usuário" });
     };
 });
 
+// TODO: Criar validação para e-mail
 router.put("/:user_id", async (req, res) => {
     const { user_id } = req.params;
     const { username, email, password, func } = req.body;
 
-    const data = {
-        username, email, password, func
-    };
-
     try {
+        let data = { username, email, password, func };
+
         await UserService.service_query_update_user_by_id(user_id, data);
         res.status(200).send({ message: "Usuário atualizado com sucesso" });
-    } catch (err) {
-        logger.error("Error fetching user:", err);
+    } catch (error) {
+        logger.error("Error fetching user:", error);
         res.status(500).send({ message: "Erro ao atualizar o usuário" });
     };
 });
@@ -63,8 +61,8 @@ router.delete("/:user_id", async (req, res) => {
     try {
         await UserService.service_query_delete_user_by_id(user_id);
         res.status(200).send({ message: "Usuário deletado com sucesso" });
-    } catch (err) {
-        logger.error("Error fetching user:", err);
+    } catch (error) {
+        logger.error("Error fetching user:", error);
         res.status(500).send({ message: "Erro ao deletar o usuário." });
     };
 });
