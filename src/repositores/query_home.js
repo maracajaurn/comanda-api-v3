0c6eda7073371cdb19c714e7c11c7c937576a371
JackSSads <contatojacksonsouza@hotmail.com>
2025-02-11 23:24:02 -0300

    corrigindo retorno do teste de conexão

--- src/repositores/query_home.js
+++ src/repositores/query_home.js
@@ -3,7 +3,12 @@ const pool = require("../../db/conn");
 const check_connection = async () => {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
-            reject(err);
+            if (err) {
+
+                reject(err);
+                return;
+            };
+
             resolve(connection);
         });
     });
