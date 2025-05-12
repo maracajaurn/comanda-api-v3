db1af1e6a9bbda40411b9bae01945c0dc7358cc0
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-12 09:55:12 -0300

    inserindo credenciais diret no arquivo via .env

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
 const admin = require("firebase-admin");
-const path = require("path");
-const serviceAccount = require(path.join(__dirname, "../../notify-comanda-avante-firebase-adminsdk.json"));
+
+const credentials = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+};
 
 class NotificationService {
     constructor() {
         this.admin = admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
+            credential: admin.credential.cert(credentials),
         });
     };
 
