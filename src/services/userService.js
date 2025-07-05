f6a6f7d30eb8e995a6829e68ff9dc0377c997a1f
JackSSads <contatojacksonsouza@hotmail.com>
2025-07-05 12:58:20 -0300

    Corrigindo lógica do serviçe usuário

--- src/services/userService.js
+++ src/services/userService.js
@@ -44,10 +44,10 @@ class UserService {
         try {
             const check_if_email_exists = await query_select_by_email(data.email);
 
-            if (check_if_email_exists[0]?.email) {
+            if (check_if_email_exists[0]) {
                 logger.error("email already exists.");
                 return { status: 409, message: "email already exists." };
-            };
+            }
 
             data.password = hashSync(data.password, 10);
             const result = await query_insert_user(data);
@@ -59,18 +59,22 @@ class UserService {
 
     async service_query_update_user_by_id(user_id, data) {
         try {
+
+            const user_if_exists = await query_select_by_id(user_id);
+
+            if (!user_if_exists[0]?.user_id) {
+                logger.error("User not found.");
+                return { status: 404, message: "user not found." };
+            };
+
             const check_if_email_exists = await query_select_by_email(data.email);
 
-            if (check_if_email_exists?.[0]?.user_id !== Number(user_id)) {
-                return { status: 409, message: "email already exists." };
+            if (check_if_email_exists[0] && check_if_email_exists[0].user_id !== Number(user_id)) {
+                logger.error("E-mail already exists.");
+                return { status: 409, message: "E-mail already exists." };
             };
 
             data.password = hashSync(data.password, 10);
-            const user_if_exists = await query_select_by_id(user_id);
-
-            if (!user_if_exists[0].user_id) {
-                return { status: 404, message: "user not found." };
-            };
 
             const result = await query_update_user_by_id(user_id, data);
             return result;
@@ -83,9 +87,9 @@ class UserService {
         try {
             const user_if_exists = await query_select_by_id(user_id);
 
-            if (!user_if_exists[0].user_id) {
+            if (!user_if_exists[0]?.user_id) {
                 return { status: 404, message: "user not found." };
-            };
+            }
 
             const result = await query_delete_user_by_id(user_id);
             return result;
@@ -98,9 +102,9 @@ class UserService {
         try {
             const user_if_exists = await query_select_by_id(user_id);
 
-            if (!user_if_exists[0].user_id) {
+            if (!user_if_exists[0]?.user_id) {
                 return { status: 404, message: "user not found." };
-            };
+            }
 
             const result = await query_insert_notify_id(user_id, notify_id);
             return result;
